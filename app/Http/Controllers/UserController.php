12858d4b53dc7b8a3eb28a554299e5989e6cf14a
mahfuzon <mahfuzon0@gmail.com>
2020-11-23 22:23:43 +0700

    API Login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,4 +25,31 @@ class UserController extends Controller
 
         return response()->json(['message'=>'Succes'], 201);
     }
+
+    public function login(Request $request){
+        $this->validate($request, [
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = User::where('email', $email)->first();
+        if(!$user){
+            return response()->json(['message' => 'login failed'], 401);
+        }
+        
+        $isValidPassword = Hash::check($password, $user->password);
+        if(!$isValidPassword){
+            return response()->json(['message' => 'login failed'], 401);
+        }
+
+        $generateToken = bin2hex(random_bytes(40));
+        $user->update([
+            'token' => $generateToken,
+        ]);
+
+        return response()->json($user);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ $router->get('/produk/{id}', 'ProdukController@show');
 $router->post('/produk', 'ProdukController@post');
 $router->put('/produk/{id}', 'ProdukController@update');
 $router->delete('/produk/{id}', 'ProdukController@destroy');
+
+$router->post('/register', 'UserController@register');
+$router->post('/login', 'UserController@login');
